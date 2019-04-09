82d9d66ea14cb98e9ca8069b73594741c448427f
Joseph Lovoi <joelovoi@gmail.com>
2019-04-09 17:13:45 -0500

    Started constructing the loop

--- review_ocr.py
+++ review_ocr.py
@@ -7,10 +7,51 @@ from wand.image import Image as Img
 
 with Img(filename='pdfs/page1.pdf', resolution=300) as img:
 	img.compression_quality = 99
-	# rotate image so it can be properly read
+	# rotate image so it can be properly read, may or may not be necessary in batch run
 	img.rotate(90)
 	img.save(filename='pdfs/page1.jpg')
 
 img = Image.open('pdfs/page1.jpg')
 
 text = pytesseract.image_to_string(img)
+
+dbdict =   {"Avg Instructor Rating In Section": 0}
+deptcount = 0
+icount = 0
+
+for line in text.splitlines():
+	print(line)
+	dbdict["Term Code"] = 201410
+	dbdict["College Code"] = "BS?"
+	"""
+	TODO: Get Term Code
+	TODO:  Get College Code
+	"""
+	if "INDIVIDUAL" in line :
+		icount += 1
+		tokens = line.split(" ")
+		dbdict["Avg Instructor Rating In Section"] += float(tokens[1])
+
+	if "Total Enrollment" in line:
+		tokens = line.split(" ")
+		dbdict["Instructor Enrollment"] = int(tokens[2])
+
+	if "Course:" in line:
+		tokens = line.split(" ")
+		dbdict["course_uuid"] = tokens[1].lower() + tokens[2][:4]
+
+	if "Instructor:" in line:
+		tokens = line.split(" ")
+		dbdict["Instructor First Name"] = tokens[1]
+		dbdict["Instructor Last Name"] = tokens[2]
+
+	if "Section Title" in line:
+		dbdict["Course Title"] = line[15:]
+
+
+
+
+
+
+
+	
